67e3202e63b613c9f81282060122d4c2dac058f2
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-01-13 12:48:35 -0300

    CSS, Json id, carrinho
    Ajuste na font-weight e color em algumas partes, detalhes no botões. Id para cada produto no arquivo json, começo da lógica para o carrinho de compra

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 let containerHTML = document.getElementById('container-grid');
 let listaDeProdutos = [];
 
+let cartHTML = document.getElementById('cart');
+let cartCounter = document.getElementById('cart-counter');
+let cart = [];
+
 const addDataToHTML = () => {
     containerHTML.innerHTML= '';
     if(listaDeProdutos.length > 0) {
         listaDeProdutos.forEach(produto => {
             let newProduto = document.createElement('div');
             newProduto.classList.add('card-dessert');
+            newProduto.dataset.id = produto.id;
             newProduto.innerHTML = `
             <img src="${produto.image.desktop}" alt="">
-            <button>Add to Cart</button>
+            <button class="addToCart">
+                <img src="assets/images/icon-add-to-cart.svg" alt="">
+                Add to Cart
+            </button>
             <p>${produto.category}</p>
             <h3>${produto.name}</h3>
             <span>$${produto.price.toFixed(2)}</span>`;
             containerHTML.appendChild(newProduto)
         })
     }
+}
+
+containerHTML.addEventListener('click', (event) => {
+    let click = event.target;
+    if(click.classList.contains('addToCart')){
+        let produto_id = click.parentElement.dataset.id;
+        addToCart(produto_id);
+        }
+})
+
+const addToCart = (produto_id) => {
+    let position = cart.findIndex((value) => value.produto_id == produto_id)
+    if(cart.length <=0) {
+        cart= [{
+            produto_id: produto_id,
+            quantity: 1
+        }]
+    } else if (position < 0) {
+        cart.push({
+            produto_id: produto_id,
+            quantity: 1
+        });
+    } else {
+        cart[position].quantity = cart[position].quantity + 1;
+    }
+    console.log(cart)
 }
 
 const initApp = () => {
